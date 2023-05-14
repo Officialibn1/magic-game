ce0272f6b762a969b9d7be4c3bb4d7760f1adabe
Officialibn1 <officialibn001@gmail.com>
2023-05-14 17:38:52 +0100

    App component modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Magic Game</h1>
+      <h1>Magic Match</h1>
+
+      <button>New Game</button>
     </div>
   );
 }
